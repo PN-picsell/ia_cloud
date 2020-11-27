3e6f7aabfe370f6c35db4dd0cd857fb0129b00bd
chakalle <v.ducros2@gmail.com>
2020-11-27 14:09:02 +0100

    updateSignin

--- ducros_valentin/django/djangoIA/ia_cloud/app_project/urls.py
+++ ducros_valentin/django/djangoIA/ia_cloud/app_project/urls.py
@@ -2,6 +2,9 @@ from django.urls import path, include
 from app_project import views
 
 urlpatterns = [
+    path('project/', views.home, name='home'),
     path('project/hello_world', views.hello_world, name="hello_world"),
-    path('project/signup', views.signup, name='signup')
+    path('project/signup', views.signup, name='signup'),
+    path('project/logout_view', views.logout_view, name='logout_view'),
+    path('project/signin', views.signin, name='signin')
 ]
--- ducros_valentin/django/djangoIA/ia_cloud/app_project/views.py
+++ ducros_valentin/django/djangoIA/ia_cloud/app_project/views.py
@@ -2,18 +2,29 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from app_project.models import Profile
 from app_project.forms import SignUpForm
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 @login_required(login_url='/signin')
 def hello_world(request):
+    print('hello_world')
     user = request.user
     return render(request, 'app_project/hello_world.html',locals())
 
+def home(request):
+    print('home')
+    user = request.user
+    if user.is_authenticated == True:
+        return render(request, 'app_project/home.html', {
+            'user': user
+        })
+    else :
+        return render(request, 'app_project/signin.html')
 
 def signup(request):
+    print('signup')
     if request.method == "POST":
         form = SignUpForm(request.POST)
         if form.is_valid():
@@ -36,3 +47,22 @@ def signup(request):
         return render(request, 'app_project/signup.html', {
             'form': form,
         })
+
+def signin(request):
+    print('signin')
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        login(request,user, backend='django.contrib.auth.backends.ModelBackend')
+        return redirect('home')
+    else:
+        print('faild authenticate')
+        return render(request, 'app_project/signin.html', {
+            'errors': "errorLogin"
+        })
+
+def logout_view(request):
+    print('logout')
+    logout(request)
+    return redirect('home')
